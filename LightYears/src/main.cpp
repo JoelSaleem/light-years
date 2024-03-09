40e67112db0650d9cd636ab6620c767a6fdc1665
JoelSaleem <40210575+JoelSaleem@users.noreply.github.com>
2024-03-09 11:23:27 +0000

    application

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <memory>
+#include <SFML/Graphics.hpp>
+#include "framework/Application.h"
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    std::unique_ptr<ly::Application> app = std::make_unique<ly::Application>();
+    app->Run();
 }

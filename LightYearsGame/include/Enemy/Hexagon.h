4f27d64ea46b1d403940718fe974f7a59a8df8d0
JoelSaleem <40210575+JoelSaleem@users.noreply.github.com>
2024-04-05 15:45:02 +0100

    :spakles: hexagon enemy

---
+++ LightYearsGame/include/Enemy/Hexagon.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Enemy/EnemySpaceship.h"
+#include "weapon/BulletShooter.h"
+
+namespace ly
+{
+    class Hexagon : public EnemySpaceship
+    {
+    public:
+        Hexagon(World *owningWorld, const std::string &texturePath = "SpaceShooterRedux/PNG/Enemies/enemyGreen4.png", const sf::Vector2f &veclocity = {0, 100.f});
+
+        virtual void Tick(float deltaTime) override;
+
+    private:
+        virtual void Shoot() override;
+        unique<BulletShooter> mShooter1;
+        unique<BulletShooter> mShooter2;
+        unique<BulletShooter> mShooter3;
+        unique<BulletShooter> mShooter4;
+        unique<BulletShooter> mShooter5;
+        unique<BulletShooter> mShooter6;
+    };
+}
